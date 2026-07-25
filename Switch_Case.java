edffefd28985d8c9115e71e6484138d6575b66f1
err0rslayer404 <02403cse050.manishsahani@knmiet.edu>
2026-07-25 23:36:48 +0530

    Switch_Case.java
    Implement a switch case to determine the day of the week based on an integer input.

---
+++ Switch_Case.java
@@ -0,0 +1,36 @@
+// Switch Case
+
+// Given the integer day denoting the day number, print on the screen which day of the week it is. Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
+
+// Ensure only the 1st letter of the answer is capitalised.
+
+
+class Solution {
+    public void whichWeekDay(int day) {
+        switch (day) {
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            case 7:
+                System.out.print("Sunday");
+                break;
+            default:
+                System.out.print("Invalid");
+        }
+    }
+}
